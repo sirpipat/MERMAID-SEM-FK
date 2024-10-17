977a78a94af732d50d9a13c23da78a8fe09aff59
Sirawich Pipatprathanporn <sirawich@princeton.edu>
2024-10-17 16:55:58 -0400

    Added trick to improve response functions by undo demean and not filter stf

--- benchmarkflatocean.m
+++ benchmarkflatocean.m
@@ -1,5 +1,5 @@
-function benchmarkflatocean(f_gaussian2d, f_gaussian3d, fc, npoles, reg, tmax)
-% BENCHMARKFLATOCEAN(f_gaussian2d, f_gaussian3d, fc, npoles, reg, tmax)
+function benchmarkflatocean(f_gaussian2d, f_gaussian3d, fc, npoles, filter_zobs, reg, tmax)
+% BENCHMARKFLATOCEAN(f_gaussian2d, f_gaussian3d, fc, npoles, filter_zobs, reg, tmax)
 %
 % Makes plots showing the benchmarking FK-SPECFEM3D to SPECFEM2D on a flat
 % bathymetry and Gaussian source-time function by reading the outputs from
@@ -14,6 +14,8 @@ function benchmarkflatocean(f_gaussian2d, f_gaussian3d, fc, npoles, reg, tmax)
 % f_gaussian3d      either 1, 2 (default), or 10
 % fc                corner frequency for low-pass filtering [default: 2]
 % npoles            number of poles for low-pass filtering  [default: 4]
+% filter_zobs       whether to apply the filter to the first arrival of
+%                   vertical displacement at ocean bottom [default: true]
 % reg               regularization method for deconvolution
 %                   either 'damp' (default) or 'water'
 % tmax              maximum time to consider for analysis and visualization
@@ -22,16 +24,18 @@ function benchmarkflatocean(f_gaussian2d, f_gaussian3d, fc, npoles, reg, tmax)
 % SEE ALSO:
 % SPECTRALDIVISION
 %
-% Last modified by sirawich-at-princeton.edu, 10/16/2024
+% Last modified by sirawich-at-princeton.edu, 10/17/2024
 
 % define more PARAMETERS here
 defval('f_gaussian2d', 1);
 defval('f_gaussian3d', 2);
 defval('fc', 1)
 defval('npoles', 4);
-defval('reg', 'damp')
-defval('tmax', 30)
+defval('filter_zobs', true);
+defval('reg', 'damp');
+defval('tmax', 30);
 ii_cutoff = 800;    % cutoff index
+correct_demean = true;
 
 % SPECFEM2D
 if f_gaussian2d == 1
@@ -67,31 +71,43 @@ t3d = tobs3d - tobs3d(1);
 zobs3d = shannon(t3d, zobs3d, t);
 pmh3d = shannon(t3d, pmh3d, t);
 
-% % compute second derivative
-% zttobs3d = diff(zobs3d, 2) / dt^2;
-% zttobs3d = [zttobs3d(1); zttobs3d; zttobs3d(end)];
-% pttmh3d = diff(pmh3d, 2) / dt^2;
-% pttmh3d = [pttmh3d(1); pttmh3d; pttmh3d(end)];
-zttobs3d = zobs3d;
-pttmh3d = pmh3d;
-
 % lowpass filtering
-% zttobs3dlp = lowpass(zttobs3d, fs, fc, npoles, 2, 'butter', 'constant');
-pttmh3dlp = lowpass(pttmh3d, fs, fc, npoles, 2, 'butter', 'constant');
-% zobs2d = lowpass(zobs2d, fs, fc, npoles, 2, 'butter', 'constant');
-pmh2d = lowpass(pmh2d, fs, fc, npoles, 2, 'butter', 'constant');
-zttobs3dlp = zttobs3d;
-% pttmh3dlp = pttmh3d;
+if fc > 0
+    % applying a filter to zobs (template, input) will perturb the nice
+    % Gaussian source-time function. 
+    if filter_zobs
+        % keeps the mean and apply after lowpass filter since it demeans the
+        zobs3d_mean = mean(zobs3d);
+        zobs2d_mean = mean(zobs2d);
+        zobs3dlp = lowpass(zobs3d, fs, fc, npoles, 2, 'butter', 'constant') + correct_demean * zobs3d_mean;
+        zobs2d = lowpass(zobs2d, fs, fc, npoles, 2, 'butter', 'constant') + correct_demean * zobs2d_mean;
+    else
+        zobs3dlp = zobs3d;
+    end
+    % keeps the mean and apply after lowpass filter since it demeans the
+    pmh3d_mean = mean(pmh3d);
+    pmh2d_mean = mean(pmh2d);
+    pmh3dlp = lowpass(pmh3d, fs, fc, npoles, 2, 'butter', 'constant') + correct_demean * pmh3d_mean;
+    pmh2d = lowpass(pmh2d, fs, fc, npoles, 2, 'butter', 'constant') + correct_demean * pmh2d_mean;
+else
+    zobs3dlp = zobs3d;
+    pmh3dlp = pmh3d;
+end
 
-% remove the trailing signals
-ztttemp = zttobs3dlp(1:ii_cutoff) .* shanning(ii_cutoff, 0.1);
-ztttemp = [ztttemp; zeros(length(zttobs3dlp)-ii_cutoff, 1)];
+% removes the trailing signals
+zobstemp = zobs3dlp(1:ii_cutoff) .* shanning(ii_cutoff, 0.1);
+zobstemp = [zobstemp; zeros(length(zobs3dlp)-ii_cutoff, 1)];
 
-% compute response functions
+% applies tiny random noise to the seismograms at the ocean bottom to avoid
+% error when calling pchave vi aspecdensplot.
+zobs2d = zobs2d + randn(size(zobs2d)) * 1e-24 * std(zobs2d);
+zobstemp = zobstemp + randn(size(zobstemp)) * 1e-24 * std(zobstemp);
+
+% computes response functions
 r2d = spectraldivision(pmh2d, zobs2d, [], reg, []);
-r3d = spectraldivision(pttmh3dlp, ztttemp, [], reg, []);
+r3d = spectraldivision(pmh3dlp, zobstemp, [], reg, []);
 
-% Make a similar plot to Fig. 8 of Pipatprathanporn & Simons (2024)
+% Makes a similar plot to Fig. 8 of Pipatprathanporn & Simons (2024)
 figure(10)
 clf
 set(gcf, 'Unit', 'inches', 'Position', [14 6.1 9 4]);
@@ -107,7 +123,7 @@ legend('MERMAID Pressure', 'OBS Z-disp 1st arrival', ...
     sprintf('Response function (%s)', reg), ...
     'Position', [0.6794 0.3535 0.1928 0.1371]);
 set(gca, 'TickDir', 'out', 'FontSize', 12)
-title(sprintf('SPECFEM2D: STF-Ricker %d Hz, lp -c %d -n %d -p 2', ...
+title(sprintf('SPECFEM2D: STF-Gaussian %d Hz, lp -c %d -n %d -p 2', ...
     f_gaussian2d, fc, npoles))
 nolabels(gca, 2)
 set(gcf, 'Renderer', 'painters')
@@ -118,10 +134,10 @@ figure(11)
 clf
 set(gcf, 'Unit', 'inches', 'Position', [14 1.1 9 4]);
 subplot('Position', [0.06 0.16 0.88 0.76])
-plot(t, pttmh3dlp / max(abs(pttmh3dlp)), 'LineWidth', 1, 'Color', [0 0.5 1]);
+plot(t, pmh3dlp / max(abs(pmh3dlp)), 'LineWidth', 1, 'Color', [0 0.5 1]);
 hold on
 grid on
-plot(t, ztttemp / max(abs(ztttemp)) - 2, 'LineWidth', 1, 'Color', [0 0.7 0]);
+plot(t, zobstemp / max(abs(zobstemp)) - 2, 'LineWidth', 1, 'Color', [0 0.7 0]);
 plot(t, r3d / max(abs(r3d)) - 4, 'LineWidth', 1, 'Color', [.75 0 0]);
 xlabel('time (s)')
 xlim([0 tmax])
@@ -169,7 +185,8 @@ ax = gca;
 ax2 = doubleaxes(ax);
 inverseaxis(ax2.XAxis, 'period (s)')
 ax2.YAxis.Label.String = ax.YAxis.Label.String;
-title(ax, 'Response function PSD', 'Position', [3.1623 135 0])
+[~, y_title] = norm2trueposition(gca, 0, 1.16);
+title(ax, 'Response function PSD', 'Position', [3.1623 y_title 0])
 set(gcf, 'Renderer', 'painters')
 figdisp(sprintf('%s_SPECFEM3D_Compare_Responses_PSD_2D_%dHz_3D_%dHz', ...
     mfilename, f_gaussian2d, f_gaussian3d), [], [], 2, [], 'epstopdf')
@@ -223,5 +240,43 @@ title(sprintf('Response function difference: SPECFEM3D (%d Hz) - SPECFEM2D (%d H
 set(gca, 'TickDir', 'out', 'FontSize', 12)
 set(gcf, 'Renderer', 'painters')
 figdisp(sprintf('%s_SPECFEM3D_Compare_Responses_TIME_2D_%dHz_3D_%dHz', ...
+    mfilename, f_gaussian2d, f_gaussian3d), [], [], 2, [], 'epstopdf')
+
+% Make a PSD plot comparing the PSD of two response functions
+figure(15)
+clf
+set(gcf, 'Unit', 'inches', 'Position', [5 11.1 9 5]);
+subplot('Position', [0.10 0.13 0.80 0.68])
+p2d = specdensplot(zobs2d(1:1500) / std(zobs2d(1:1500)), 500, fs, 500, 70, 10, 's');
+hold on
+grid on
+p3d = specdensplot(zobstemp(1:1500) / std(zobstemp(1:1500)), 500, fs, 500, 70, 10, 's');
+for ii = 1:length(p2d)
+    p2d(ii).Color = [0 0 0];
+    p3d(ii).Color = [.75 0 0];
+    if ii == 1
+        p2d(ii).LineWidth = 1;
+        p3d(ii).LineWidth = 1;
+    elseif ii == 4
+        p2d(ii).MarkerFaceColor = p2d(ii).Color;
+        p3d(ii).MarkerFaceColor = p3d(ii).Color;
+        p2d(ii).Marker = 'o';
+        p3d(ii).Marker = 'o';
+        p2d(ii).MarkerSize = 4;
+        p3d(ii).MarkerSize = 4;
+    end
+end
+set(gca, 'TickDir', 'out', 'FontSize', 12, 'XTick', [.2 1 10 49.99], ...
+    'XTickLabel', {'0.2', '1', '10', '50'})
+legend([p2d(1) p3d(1)], sprintf('SPECFEM2D (%d Hz)', f_gaussian2d), ...
+    sprintf('SPECFEM3D (%d Hz)', f_gaussian3d))
+ax = gca;
+ax2 = doubleaxes(ax);
+inverseaxis(ax2.XAxis, 'period (s)')
+ax2.YAxis.Label.String = ax.YAxis.Label.String;
+[~, y_title] = norm2trueposition(ax, 0.50, 1.16);
+title(ax, 'OBS Z-disp function PSD', 'Position', [3.1623 y_title 0])
+set(gcf, 'Renderer', 'painters')
+figdisp(sprintf('%s_SPECFEM3D_Compare_zdisp_PSD_2D_%dHz_3D_%dHz', ...
     mfilename, f_gaussian2d, f_gaussian3d), [], [], 2, [], 'epstopdf')
 end
